d2ecc869c50c22bbf210ea3c69261ba0bb13f84e
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2025-04-14 23:06:17 -0300

    added menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject creditsButton;
+    public GameObject carroButton;
+    public GameObject sapoButton;
+    public GameObject backButton;
+    public GameObject creditsText;
+
+    public void StartGame()
+    {
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+        carroButton.SetActive(true);
+        sapoButton.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+        backButton.SetActive(true);
+        creditsText.SetActive(true);
+    }
+
+    public void RunCarro()
+    {
+        // Load the Carro game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Road-trip");
+    }
+
+    public void RunSapo()
+    {
+        // Load the Sapo game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Frogger");
+    }
+
+    public void Back() {
+        startButton.SetActive(true);
+        creditsButton.SetActive(true);
+        carroButton.SetActive(false);
+        sapoButton.SetActive(false);
+        backButton.SetActive(false);
+        creditsText.SetActive(false);
+    }
+}
