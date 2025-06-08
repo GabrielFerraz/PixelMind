707b6c781da9569cbc4504591fff34778a53e0b3
GabrielFerraz <gabrielmaiaferraz@gmail.com>
2025-06-08 10:59:36 -0300

    frogger all assets

--- Assets/Scripts/Score/ScoreController.cs
+++ Assets/Scripts/Score/ScoreController.cs
@@ -8,7 +8,7 @@ using TMPro;
 public class ScoreController : MonoBehaviour
 {
     public ScoreComponent[] scores;
-    public GameObject lineChart;
+    public LineChart lineChart;
     public TextMeshProUGUI levelNameText;
     private GameData gameData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,21 +17,30 @@ public class ScoreController : MonoBehaviour
         GameData gameData = SaveSystem.Load();
         levelNameText.text = gameData.currentLevelName;
         var level = gameData.currentLevel;
-        level = level.OrderByDescending(x => x.score).ToList();
+        var orderedLevel = level.AsEnumerable().OrderByDescending(x => x.score).ToList();
+        var reversed = level.TakeLast(5).ToList();
+        lineChart.RemoveData();
+        lineChart.AddSerie<Line>("Score");
         for (int i = 0; i < 5; i++)
         {
             var scoreComponent = scores[i];
-            if (level == null || i < level.Count)
+            if (orderedLevel == null || i < orderedLevel.Count)
             {
-                var score = level[i].score;
+                var score = orderedLevel[i].score;
                 scoreComponent.scoreText.text = score.ToString();
-                scoreComponent.trophy.SetActive(level[i].timestamp == gameData.currentTimestamp);
-                scoreComponent.bg.SetActive(level[i].timestamp == gameData.currentTimestamp);
+                scoreComponent.trophy.SetActive(orderedLevel[i].timestamp == gameData.currentTimestamp);
+                scoreComponent.bg.SetActive(orderedLevel[i].timestamp == gameData.currentTimestamp);
             }
+            double[] data = new double[2];
+            data[0] = i;
+            data[1] = reversed[i].score;
+            lineChart.AddData(0, data);
         }
-        // var chart = gameObject.GetComponent<LineChart>();
-        // chart.ClearData();
         
+        // lineChart.AddData(0, serie);
+        // var chart = gameObject.GetComponent<LineChart>();
+
+
         // var graphData = gameData.currentLevel.OrderBy(x => DateTime.TryParse(x.timestamp, out DateTime parsedTimestamp)).Take(5).ToList();
         // for (int i = 0; i < graphData.Count; i++)
         // {
